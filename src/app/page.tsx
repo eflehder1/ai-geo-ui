b42427a2051e37fda54d93b878bae95af9f58688
Eric Lehder <eric.lehder@gmail.com>
2025-08-12 20:30:34 +0100

    new yml

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,6 @@
 
 import { useState } from "react";
 
-import dynamic from "next/dynamic";
-const STLViewer = dynamic(() => import("../components/STLViewer"), { ssr: false });
-
-
 export default function Home() {
     const [prompt, setPrompt] = useState("");
     const [loading, setLoading] = useState(false);
